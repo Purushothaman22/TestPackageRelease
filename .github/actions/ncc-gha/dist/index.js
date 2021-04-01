b3073308feb222b7bf7e243205ae3c9225472c4f
Purushothaman Bhaskar <purushothaman.b@surya-soft.com>
2021-04-01 17:40:20 +0530

    Push to same branch

--- .github/actions/ncc-gha/dist/index.js
+++ .github/actions/ncc-gha/dist/index.js
@@ -37,7 +37,7 @@ async function run() {
                 await exec.exec('git', ['commit', '-m', 'Use  @vercel/ncc']);
                 const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}`.replace(/^https:\/\//, `https://x-access-token:${token}@`);
     
-                await exec.exec('git', ['push', url, 'HEAD']);
+                await exec.exec('git', ['push', url]);
                 
             });
         } else {
