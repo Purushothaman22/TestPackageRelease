d2864ec0e3974ab4ae40719cb552898919b67f36
Purushothaman Bhaskar <purushothaman.b@surya-soft.com>
2021-04-02 15:52:28 +0530

    Change remote label

--- .github/actions/ncc-gha/dist/index.js
+++ .github/actions/ncc-gha/dist/index.js
@@ -25,9 +25,9 @@ async function run() {
         const branch = github.context.payload.pull_request.head.ref;
 
         const git = simpleGit();
-        await git.init().addRemote('origin', url);
-        await git.fetch('origin')
-        await git.checkout(`origin/${branch}`)
+        await git.init().addRemote('repo', url);
+        await git.fetch('repo')
+        await git.checkout(`repo/${branch}`)
 
         await exec.exec('npm install');
         await exec.exec('npm i @vercel/ncc');
@@ -46,7 +46,7 @@ async function run() {
                 await exec.exec('git', ['commit', '-m', 'Use  @vercel/ncc']);
                 
 
-                await git.push(`origin/${branch}`);
+                await git.push(`repo/${branch}`);
                 
             });
         } else {
