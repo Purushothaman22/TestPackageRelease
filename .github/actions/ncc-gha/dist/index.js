fc2f5f723ee2bf560467ca2b85f3ef081d7255c1
Purushothaman Bhaskar <purushothaman.b@surya-soft.com>
2021-04-02 09:00:30 +0530

    Reset push code

--- .github/actions/ncc-gha/dist/index.js
+++ .github/actions/ncc-gha/dist/index.js
@@ -21,14 +21,10 @@ async function run() {
 
     try {
         const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}`.replace(/^https:\/\//, `https://x-access-token:${token}@`);
-        await exec.exec('git branch')
 
         await exec.exec('npm install');
         await exec.exec('npm i @vercel/ncc');
         await exec.exec('./node_modules/@vercel/ncc/dist/ncc/cli.js', ['build', mainFilePath, '--license', 'licenses.txt']);
-        const branch = github.context.payload.pull_request.head.ref;
-        await exec.exec('git',['pull', url, branch])
-    
         // check for git diff
         const diff = await exec.exec(
             'git', ['diff', '--quiet'], {ignoreReturnCode: true}
@@ -36,6 +32,9 @@ async function run() {
 
         if (diff) {
             await core.group('push changes', async () => {
+                const branch = github.context.payload.pull_request.head.ref;
+                await exec.exec('git', ['checkout', 'HEAD', '-b', branch]);
+
                 const actor = env.GITHUB_ACTOR
                 await exec.exec('git', ['config', 'user.name', actor]);
 
@@ -44,7 +43,7 @@ async function run() {
                 await exec.exec('git', ['commit', '-m', 'Use  @vercel/ncc']);
                 const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}`.replace(/^https:\/\//, `https://x-access-token:${token}@`);
     
-                await exec.exec('git', ['push', url]);
+                await exec.exec('git', ['push', url, 'HEAD']);
 
             });
         } else {
