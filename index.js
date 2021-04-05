cd8ab1730b59acb5464a7f26fbc854f55499daae
Purushothaman Bhaskar <purushothaman.b@surya-soft.com>
2021-04-05 10:27:17 +0530

    Run action on master

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
+const exec = require('@actions/exec');
 const core = require('@actions/core');
-const token = core.getInput('TOKEN');
+const github = require('@actions/github')
+const simpleGit = require('simple-git');
+const fs = require('fs');
 
-console.log(token);
+const env = process.env;
 
-var g1 = new Date("2021-03-26T11:15:58Z");
-console.log(`g1: ${g1.getDate()}`);
-// (YYYY, MM, DD, Hr, Min, Sec)
-var g2 = new Date("2021-04-27T11:15:58Z");
+async function run() {
+    const token = core.getInput('TOKEN');
+    var mainFilePath = core.getInput('MAIN_FILE_PATH')
 
-//calculate time difference  
-var time_difference = g1.getTime() - g2.getTime();  
-  
-//calculate days difference by dividing total milliseconds in a day  
-var days_difference = time_difference / (1000 * 60 * 60 * 24);  
+    if (!mainFilePath) {
+        mainFilePath = './index.js'
+    }
+
+    try {
+        const url = `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}.git`.replace(/^https:\/\//, `https://x-access-token:${token}@`);
+        const branch = github.context.payload.pull_request.head.ref;
+
+        const git = simpleGit();
+        await git.addRemote('repo', url);
+        await git.fetch('repo')
+        await git.checkout(branch)
+
+        const distFolderAlreadyExists = fs.existsSync('./dist');
+        await exec.exec('npm install');
+        await exec.exec('npm i @vercel/ncc');
+        await exec.exec('./node_modules/@vercel/ncc/dist/ncc/cli.js', ['build', mainFilePath, '--license', 'licenses.txt']);
+        
+
+        // check for git diff
+        const diff = await exec.exec(
+            'git', ['diff', '--quiet'], {ignoreReturnCode: true}
+        );
+    
+        if (diff || !distFolderAlreadyExists) {
+            await core.group('push changes', async () => {
+                await git.addConfig('user.email', `${env.GITHUB_ACTOR}@users.noreply.github.com`)
+                await git.addConfig('user.name', env.GITHUB_ACTOR)
+                await git.add('./dist')
+                await git.commit("Use  @vercel/ncc")
+                await git.push('repo', branch);
+            });
+        } else {
+            console.log("Node js module is up to date.");
+        }
+    } catch(error) {
+        core.setFailed(error);
+    }
+}
+
+run();
